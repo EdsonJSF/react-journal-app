d027e20566764fddfe45fa86e3d496a8be277d2c
EdsonJSF <EdsonJSF1990@gmail.com>
2023-12-18 14:57:08 -0400

    Feat: IconButton

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,6 @@
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
+
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView } from "../views/NoteView";
 import { NothingSelectedView } from "../views/NothingSelectedView";
@@ -5,8 +8,22 @@ import { NothingSelectedView } from "../views/NothingSelectedView";
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelectedView /> */}
-      <NoteView/>
+      <NothingSelectedView />
+      {/* <NoteView/> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.8 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
