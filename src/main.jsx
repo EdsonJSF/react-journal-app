a322e1b43eb6aee8c89aa3750f9f11186630fc35
EdsonJSF <edsonjsf1990@gmail.com>
2023-12-28 13:48:21 -0400

    Feat: Configure react-redux-toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+/* Redux tookit */
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 import "./main.css";
 
 import { JournalApp } from "./JournalApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
