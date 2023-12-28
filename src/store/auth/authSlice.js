f5827081bdfa524a70c501c30132174b4516ac38
EdsonJSF <edsonjsf1990@gmail.com>
2023-12-28 14:28:09 -0400

    Feat: Configure Auth Slice

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  status: "checking",
+  uid: null,
+  email: null,
+  displayName: null,
+  photoURL: null,
+  errorMessage: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {},
+    logout: (state, action) => {},
+    checkingCredentials: (state) => {},
+  },
+});
+
+export const { login, logout, checkingCredentials } = authSlice.actions;
