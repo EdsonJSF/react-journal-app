138603142b830d184d1f522dbb2c85467589934f
EdsonJSF <edsonjsf1990@gmail.com>
2024-02-01 21:55:52 -0400

    feat: preserve user authentication

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,33 @@
-import { Route, Routes } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
+/* FireBase */
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "../firebase/config";
+
+/* Store */
+import { status as authStatus, login, logout } from "../store/auth";
+
+/* Routes Components */
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
 
-import { status as authStatus } from "../store/auth";
+/* Components */
 import { CheckingAuth } from "../ui";
 
 export const AppRouter = () => {
   const { status } = useSelector((state) => state.authReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, async (user) => {
+      if (!user) return dispatch(logout);
+
+      const { uid, email, displayName, photoURL } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+    });
+  }, []);
 
   if (status === authStatus[0]) {
     return <CheckingAuth />;
@@ -17,11 +36,16 @@ export const AppRouter = () => {
   if (status)
     return (
       <Routes>
-        {/* Login & Register */}
-        <Route path="/auth/*" element={<AuthRoutes />} />
+        {
+          (status === authStatus[1] )
+          /* JournalApp */
+          ? <Route path="/*" element={<JournalRoutes />} />
 
-        {/* JournalApp */}
-        <Route path="/*" element={<JournalRoutes />} />
+          /* Login & Register */
+          : <Route path="/auth/*" element={<AuthRoutes />} />
+        }
+
+        <Route path="/*" element={<Navigate to="/auth/login" />} />
       </Routes>
     );
 };
