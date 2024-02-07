ceb98211bd0d0ef245ee1e858cb54d3d39818516
EdsonJSF <edsonjsf1990@gmail.com>
2024-02-06 23:23:02 -0400

    feat (journal firestore): create new notes

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,19 @@
+import { useDispatch } from "react-redux";
 import { IconButton } from "@mui/material";
 import { AddOutlined } from "@mui/icons-material";
 
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView } from "../views/NoteView";
 import { NothingSelectedView } from "../views/NothingSelectedView";
+import { startNewNote } from "../../store/journal";
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+
+  const handleNewNote = () => {
+    dispatch(startNewNote());
+  };
+
   return (
     <JournalLayout>
       <NothingSelectedView />
@@ -21,6 +29,7 @@ export const JournalPage = () => {
           right: 50,
           bottom: 50,
         }}
+        onClick={handleNewNote}
       >
         <AddOutlined sx={{ fontSize: 30 }} />
       </IconButton>
--- src/store/journal/journalThunks.js
+++ src/store/journal/journalThunks.js
@@ -1,13 +1,20 @@
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { firestoreApp } from "../../firebase/config";
+
 export const startNewNote = () => {
-  return async (dispatch) => {
-    // uid
+  return async (dispatch, getState) => {
+    const { uid } = getState().authReducer;
 
     const newNote = {
       title: "",
       body: "",
       date: new Date().getTime(),
-      imageUrls: [],
     };
+
+    const newDoc = doc(collection(firestoreApp, `${uid}/journal/notes`));
+    const resp = await setDoc(newDoc, newNote);
+
+    console.log(newDoc, resp);
 
     // TODO: Create dispatch
     // dispatch (newNote)
