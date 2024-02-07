b784138238b88b0710aa9f4c3d2b96644014e24a
EdsonJSF <edsonjsf1990@gmail.com>
2024-02-06 22:54:56 -0400

    store (journal): prepare thunks

---
+++ src/store/journal/journalThunks.js
@@ -0,0 +1,16 @@
+export const startNewNote = () => {
+  return async (dispatch) => {
+    // uid
+
+    const newNote = {
+      title: "",
+      body: "",
+      date: new Date().getTime(),
+      imageUrls: [],
+    };
+
+    // TODO: Create dispatch
+    // dispatch (newNote)
+    // dispatch (activeNote)
+  };
+};
