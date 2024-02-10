a9907bf231e6b4337451a49fd1061414b5566dec
EdsonJSF <edsonjsf1990@gmail.com>
2024-02-09 22:54:12 -0400

    feat: activate note for editing

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,8 +1,23 @@
-import { SaveOutlined } from "@mui/icons-material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
 import { Button, Grid, TextField, Typography } from "@mui/material";
+import { SaveOutlined } from "@mui/icons-material";
+
+import { useForm } from "../../hooks";
 import { ImageGallery } from "../components";
 
 export const NoteView = () => {
+  const { active: activeNote } = useSelector((state) => state.journalReducer);
+
+  const { body, title, date, handleInputChange, formState } =
+    useForm(activeNote);
+
+  const dateString = useMemo(() => {
+    const newDate = new Date(date);
+
+    return newDate.toUTCString();
+  }, [date]);
+
   return (
     <Grid
       container
@@ -13,7 +28,7 @@ export const NoteView = () => {
       className="animate__animated animate__fadeIn animate__faster"
     >
       <Typography fontSize={39} fontWeight="light">
-        December 23, 2023
+        {dateString}
       </Typography>
 
       <Grid item>
@@ -31,6 +46,9 @@ export const NoteView = () => {
           placeholder="Insert a title"
           label="Title"
           sx={{ border: "none", mb: 1 }}
+          name="title"
+          value={title}
+          onChange={handleInputChange}
         />
 
         <TextField
@@ -41,6 +59,9 @@ export const NoteView = () => {
           minRows={5}
           placeholder="Whatsup today"
           sx={{ border: "none", mb: 1 }}
+          name="body"
+          value={body}
+          onChange={handleInputChange}
         />
       </Grid>
 
